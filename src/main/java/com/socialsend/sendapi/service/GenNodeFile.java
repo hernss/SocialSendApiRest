1e9b84113435529087a0b009c629dafbb54f7040
hernss <hernantravado@yahoo.com.ar>
2018-04-01 15:02:40 -0300

    Generate method Downlaod File

--- src/main/java/com/socialsend/sendapi/service/GenNodeFile.java
+++ src/main/java/com/socialsend/sendapi/service/GenNodeFile.java
@@ -1,6 +1,61 @@
 package com.socialsend.sendapi.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
+
+import com._37coins.bcJsonRpc.pojo.PeerInfo;
+import com.socialsend.sendapi.rpc.RPCConnection;
+
+
+@Path("/nodefile")
 public class GenNodeFile {
 	
+	@GET
+	@Path("/download")
+	@Produces("text/plain")
+	public Response downloadFile() {
+		RPCConnection rpc = RPCConnection.getInstance();
+		 
+		try {
+			@SuppressWarnings("unused")
+			List<PeerInfo> r = rpc.getpeerinfo();
+		} catch (Exception e) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
 
+		StreamingOutput fileStream =  new StreamingOutput()
+        {
+            
+            public void write(java.io.OutputStream output)
+            {
+            	RPCConnection rpc = RPCConnection.getInstance();
+            	for(PeerInfo p: rpc.getpeerinfo()) {
+        			String line = "send-cli addnode " + p.getAddr() + " add\r\n";
+        			
+        			try {
+        				output.write(line.getBytes());
+						output.flush();
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+        		}
+                
+            }
+        };
+        return Response
+                .ok(fileStream, MediaType.APPLICATION_OCTET_STREAM)
+                .header("content-disposition","attachment; filename = addnode.sh")
+                .build();
+		
+		
+	}
 }
