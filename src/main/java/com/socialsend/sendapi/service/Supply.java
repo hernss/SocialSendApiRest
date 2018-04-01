f6bb08e75768edc80b350860dc29c00b072d9770
hernss <hernantravado@yahoo.com.ar>
2018-04-01 17:08:32 -0300

    Add method getaverageblocktime

--- src/main/java/com/socialsend/sendapi/service/Supply.java
+++ src/main/java/com/socialsend/sendapi/service/Supply.java
@@ -1,10 +1,14 @@
 package com.socialsend.sendapi.service;
 
 
+import java.math.BigDecimal;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com._37coins.bcJsonRpc.pojo.Block;
 import com.socialsend.sendapi.response.Response;
 import com.socialsend.sendapi.rpc.RPCConnection;
 
@@ -28,4 +32,25 @@ public class Supply {
 		return r;
 	}
 	
+	@GET
+	@Path("/getaverageblocktime")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response<Double> getaverageblocktime() {
+		RPCConnection rpc = RPCConnection.getInstance();
+		Response<Double> r ;
+		try {
+			Block bestBlock  = rpc.getblock(rpc.getbestblockhash());
+			Block block  = rpc.getblock(rpc.getblockhash(new BigDecimal(bestBlock.getHeight() - 1000)));
+			long time = bestBlock.getTime() - block.getTime();
+			double avg = (double)time / 1000.0;
+			r = new Response<Double>(avg);
+			r.setMessage("Average blocktime in seconds of last 1000 blocks");
+		} catch (Exception e) {
+			r = new Response<Double>(null);
+			r.setStatus("ERROR");
+			r.setMessage(e.getMessage());
+		}
+		
+		return r;
+	}
 }
