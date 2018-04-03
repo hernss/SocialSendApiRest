319eb205398c46d3b5c267a144c05fd0d73ed810
hernss <hernantravado@yahoo.com.ar>
2018-04-03 18:11:34 -0300

    Fix bug on getvalue method

--- src/main/java/com/socialsend/sendapi/service/Transactions.java
+++ src/main/java/com/socialsend/sendapi/service/Transactions.java
@@ -16,9 +16,9 @@ import com.socialsend.sendapi.rpc.RPCConnection;
 public class Transactions {
 
 	@GET
-	@Path("/getvalue/{txid}")
+	@Path("/getvalue/{txid}/{addr}")
 	@Produces({MediaType.APPLICATION_JSON})
-	public Response<Double> getvalue(@PathParam("txid") String txid) {
+	public Response<Double> getvalue(@PathParam("txid") String txid, @PathParam("addr") String addr) {
 		RPCConnection rpc = RPCConnection.getInstance();
 		Response<Double> r ;
 		try {
@@ -34,50 +34,63 @@ public class Transactions {
 					//Consulto los valores de salida
 					double valuePOS = 0;
 					double valueMN = 0;
-					String addressDestino = "";
+					boolean isMasternode = false;
 					for(Vout v : tx1.getVout()) {
 						if(v.getScriptPubKey().getType().equals("pubkey")) {
-							valuePOS += v.getValue();
-							
-							if(!v.getScriptPubKey().getAddresses().get(0).equals("")) {
-								addressDestino = v.getScriptPubKey().getAddresses().get(0);
+							if(v.getScriptPubKey().getAddresses().get(0).equals(addr)) {
+								valuePOS += v.getValue();
 							}
 						}
 						if(v.getScriptPubKey().getType().equals("pubkeyhash")) {
 							valueMN += v.getValue();
+							if(v.getScriptPubKey().getAddresses().get(0).equals(addr)) {
+								isMasternode = true;
+							}
 						}
 				
 					}
 					
-					//Consulto los valores de entrada.
-					if(!tx1.getVin().isEmpty()) {
-						String txidEntrada = tx1.getVin().get(0).getTxid();
-						long vout = tx1.getVin().get(0).getVout();
-						
-						//Busco la tx de origen
-						DecodedTransaction tx2 = rpc.decoderawtransaction(rpc.getrawtransaction(txidEntrada));
-						
-						if((!tx2.getVout().isEmpty()) && tx2.getVout().get((int)vout).getScriptPubKey().getAddresses().get(0).equals(addressDestino)) {
-							double valuePOSentrada = tx2.getVout().get((int)vout).getValue();
-							r = new Response<Double>(valuePOS - valuePOSentrada);
+					if(isMasternode) {
+						r = new Response<Double>(valueMN);
+						r.setMessage("MN Reward");
+					}else {
+
+						//Consulto los valores de entrada.
+						if(!tx1.getVin().isEmpty()) {
+							String txidEntrada = tx1.getVin().get(0).getTxid();
+							long vout = tx1.getVin().get(0).getVout();
+							
+							//Busco la tx de origen
+							DecodedTransaction tx2 = rpc.decoderawtransaction(rpc.getrawtransaction(txidEntrada));
+							
+							if((!tx2.getVout().isEmpty()) && tx2.getVout().get((int)vout).getScriptPubKey().getAddresses().get(0).equals(addr)) {
+								double valuePOSentrada = tx2.getVout().get((int)vout).getValue();
+								r = new Response<Double>(valuePOS - valuePOSentrada);
+								r.setMessage("Stake Reward");
+							}else {
+								r = new Response<Double>(null);
+								r.setStatus("ERROR");
+								r.setMessage("No se encontraron Vouts validos");
+							}
 						}else {
 							r = new Response<Double>(null);
 							r.setStatus("ERROR");
-							r.setMessage("No se encontraron Vouts validos");
+							r.setMessage("No se encontraron Vins validos");
 						}
-					}else {
-						r = new Response<Double>(null);
-						r.setStatus("ERROR");
-						r.setMessage("No se encontraron Vins validos");
 					}
-					
 				}else {
 					//TX p2p
 					System.out.println("TXID-P2P");
-					double value = tx1.getVout().get(0).getValue();
-					String addrDestino = sp.getAddresses().get(0);
+					double value = 0;
+					
+					for(Vout v : tx1.getVout()) {
+						if (v.getScriptPubKey().getAddresses().get(0).equals(addr)) {
+							value = v.getValue();
+						}
+					}
+					
 					r = new Response<Double>(value);
-					r.setMessage("AddrTo:" + addrDestino);
+					r.setMessage("P2P Send");
 				}
 			}else {
 				r = new Response<Double>(null);
