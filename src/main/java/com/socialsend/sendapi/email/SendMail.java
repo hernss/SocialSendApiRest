a72d23ea578964c3b896384637d1dc0ae69810d8
hernss <hernantravado@yahoo.com.ar>
2018-04-05 23:01:54 -0300

    Add method validateEmail

--- src/main/java/com/socialsend/sendapi/email/SendMail.java
+++ src/main/java/com/socialsend/sendapi/email/SendMail.java
@@ -3,7 +3,8 @@ package com.socialsend.sendapi.email;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SendMail {
 
@@ -82,4 +83,13 @@ public class SendMail {
         return true;
     }
 
+    public boolean validateEmail(String email) {
+    	Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+ 
+        Matcher matcher = pattern.matcher(email);
+ 
+        return matcher.find();
+    }
 }
