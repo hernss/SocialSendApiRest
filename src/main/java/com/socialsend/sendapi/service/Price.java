eadeff73bd9bfffffd56ad8a7ba79f94881c702e
hernss <hernantravado@yahoo.com.ar>
2018-04-07 22:20:10 -0300

    Finish method bot to get data for bot running

--- src/main/java/com/socialsend/sendapi/service/Price.java
+++ src/main/java/com/socialsend/sendapi/service/Price.java
@@ -1,5 +1,7 @@
 package com.socialsend.sendapi.service;
 
+import java.math.BigDecimal;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,6 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com._37coins.bcJsonRpc.pojo.Block;
+import com._37coins.bcJsonRpc.pojo.BotResponse;
 import com.socialsend.sendapi.database.Database;
 import com.socialsend.sendapi.response.Response;
 import com.socialsend.sendapi.rpc.RPCConnection;
@@ -103,6 +106,58 @@ public class Price {
 		return r;
 	}
 	
-	
+	@GET
+	@Path("/bot")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response<BotResponse> bot() {
+		
+		/*
+		 *	priceBTC X
+		 *	priceUSD X
+		 * 	Buy Volume X
+		 * 	Sell volume X
+		 * 	MasternodeCounts
+		 * 	Masternode Value
+		 * 	CoinsLocked (%)
+		 * 	Daily Income 
+		 *  ROI
+		 *  
+		 */
+		Database db = Database.getInstance();
+		RPCConnection rpc = RPCConnection.getInstance();
+		
+		Response<BotResponse> r = null;
+		BotResponse br = null;
+		try {
+			br = db.getBotResponse();
+			
+			br.setMasternodesCount(rpc.getMasternodeCount().getTotal().longValue());
+			br.setMasternodeValue(br.getMasternodesCount() * br.getPriceUSD());
+			br.setCoinsLocked(br.getMasternodesCount() * 6250);
+			double TotalSupply = rpc.gettxoutsetinfo().getTotalSupply();
+			br.setCoinsLockedPor(br.getCoinsLocked() / TotalSupply * 100);
+			
+			long secondsInOneDay = 86400;
+			
+			Block bestBlock  = rpc.getblock(rpc.getbestblockhash());
+			Block block  = rpc.getblock(rpc.getblockhash(new BigDecimal(bestBlock.getHeight() - 1000)));
+			long time = bestBlock.getTime() - block.getTime();
+			double avgBlockTime = (double)time / 1000.0;
+			
+			double avgBlocksPerDay = secondsInOneDay / avgBlockTime;
+			double avgRewardsPerDay = (avgBlocksPerDay * 3.99999999) / br.getMasternodesCount();
+			br.setDailyIncome(avgRewardsPerDay);
+			br.setROI(((avgRewardsPerDay * 365) / 6250) * 100);
+			
+		} catch (Exception e) {
+			r = new Response<BotResponse>(null);
+			r.setStatus("ERROR");
+			r.setMessage("Error when trying to read DB.");
+			return r;
+		}
+		
+		r = new Response<BotResponse>(br);
+		return r;
+	}
 	
 }
